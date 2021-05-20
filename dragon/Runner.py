bd7115d6ac3871a92ab59d0110c7a97c35e3daf9
Olivia <zoe.i2k1@gmail.com>
2021-05-20 12:23:30 +0200

    And we have an OAuth flow!

--- dragon/Runner.py
+++ dragon/Runner.py
@@ -1,6 +1,13 @@
 import os
+from sys import platform
 
-import StackAPI
+import stackapi as API
+
+# Static validation {{{
+if platform != "linux":
+    print(platform, "isn't supported")
+    exit(-1)
+# }}}
 
 # Static {{{
 CLIENT_ID = "20280"
@@ -13,8 +20,15 @@ OAUTH_VERIFICATION_URL = "https://lunarwatcher.github.io/Dragon/token_echo.html"
 # Token management {{{
 oauthToken = ""
 
-if !os.path.isfile(".oauth.txt"):
-    pass
+if not os.path.isfile(".oauth.txt"):
+    oauthURL = f"https://stackoverflow.com/oauth/dialog?client_id={CLIENT_ID}\&scope=write_access,no_expiry\&redirect_uri={OAUTH_VERIFICATION_URL}"
+    print("Opening", oauthURL)
+    os.system(f"xdg-open {oauthURL}")
+    print("Client-side OAuth flow launched")
+    oauthToken = input("Paste your token here: ")
+    with open(".oauth.txt", "w") as f:
+        f.write(oauthToken)
+
 else:
     with open(".oauth.txt", "r") as f:
         oauthToken = f.readlines()[0]
@@ -25,8 +39,6 @@ if (oauthToken == ""):
 # }}}
 # Token verification {{{
 print("OAuth token loaded. Testing a dry run against the API...")
-
-oauthURL = f"https://stackoverflow.com/oauth?client_id={CLIENT_ID}"
 
 # }}}
 
